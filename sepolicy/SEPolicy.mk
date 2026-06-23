c984fea2f0f58bf689d4700be29f5e00a4f70f3f
gaurav-paul9 <paulgaurav9@gmail.com>
2026-06-23 11:30:14 +0530

    Revert SEPolicy.mk — type declared in vndr instead

--- sepolicy/SEPolicy.mk
+++ sepolicy/SEPolicy.mk
@@ -5,8 +5,10 @@
 #
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    vendor/oplus/camera/sepolicy/vendor \
-    vendor/oplus/camera/sepolicy/public
+    vendor/oplus/camera/sepolicy/vendor
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     vendor/oplus/camera/sepolicy/private
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    vendor/oplus/camera/sepolicy/public
