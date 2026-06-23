9ebd2851c4698883201dcd9c0cd5c7a111b5a307
gaurav-paul9 <paulgaurav9@gmail.com>
2026-06-23 11:13:07 +0530

    sepolicy: Move public/ type decl to vendor scope for domain.te reference
    domain.te in sepolicy_vndr references opluscamera_app in neverallow
    exceptions. The type declaration must be visible to vendor sepolicy,
    not just system_ext.

--- sepolicy/SEPolicy.mk
+++ sepolicy/SEPolicy.mk
@@ -5,10 +5,8 @@
 #
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    vendor/oplus/camera/sepolicy/vendor
+    vendor/oplus/camera/sepolicy/vendor \
+    vendor/oplus/camera/sepolicy/public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     vendor/oplus/camera/sepolicy/private
-
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    vendor/oplus/camera/sepolicy/public
